f6c345129921e3a06012c26611fd2957a45dd5e2
Basantakhanal <basanta4965@gmail.com>
2026-02-16 18:49:59 +0545

    started

---
+++ my-project/src/App.jsx
@@ -0,0 +1,29 @@
+import Experience from "./Sections/Experience";
+
+import Testimonials from "./Sections/Testimonials";
+import Navbar from "./Components/Navbar";
+import Home from "./Sections/Home";
+import Skills from "./Sections/Skills";
+import Contact from "./Sections/Contact";
+
+import Project from "./Sections/Project";
+import Footers from "./Sections/Footers";
+import ParticlesBackgrounds from "./Components/ParticlesBackgrounds";
+
+
+
+
+  export default function App(){
+  return( 
+ <div className="relative min-h-screen bg-gradient-to-br from-[#302b63] via-[#00bf8f] to-[#1cd8d2] text-white">
+<ParticlesBackgrounds></ParticlesBackgrounds>
+    <Navbar></Navbar>
+    <Home></Home>
+    <Skills></Skills>
+    <Project></Project>
+    <Experience></Experience>
+    <Testimonials></Testimonials>
+    <Contact></Contact>
+    <Footers></Footers>
+  </div>)
+}
